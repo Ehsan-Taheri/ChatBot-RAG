78ec577150e3c8c9126c244a635aa0be8dffae8f
Ehsan Taheri <ehsan.taheri1400@gmail.com>
2024-06-15 20:11:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,225 +1,112 @@
-import streamlit as st
 import os
-import falcon
-import torch
-import time
-import gc
-#from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo
-#progress_bar
-#main_placeholder = st.empty()
-#def main_place(message="The Task Is Finished !!!!"):
-  # main_placeholder.text(message)
+import streamlit as st
+from document_processing import read_pdf, read_txt, split_doc, embedding_storing
+from chatbot import prepare_rag_llm, generate_answer
 
-# Memory management functions
-def clear_gpu_memory():
-    torch.cuda.empty_cache()
-    gc.collect()
-
-def wait_until_enough_gpu_memory(min_memory_available, max_retries=10, sleep_time=5):
-    nvmlInit()
-    handle = nvmlDeviceGetHandleByIndex(torch.cuda.current_device())
-
-    for _ in range(max_retries):
-        info = nvmlDeviceGetMemoryInfo(handle)
-        if info.free >= min_memory_available:
-            break
-        print(f"Waiting for {min_memory_available} bytes of free GPU memory. Retrying in {sleep_time} seconds...")
-        time.sleep(sleep_time)
-    else:
-        raise RuntimeError(f"Failed to acquire {min_memory_available} bytes of free GPU memory after {max_retries} retries.")
+def load_secrets():
+    api_key = st.text_input("Enter your API Key:", type='password')
+    return api_key
 
 def main():
-    # Call memory management functions before starting Streamlit app
-    #min_memory_available = 1 * 1024 * 1024 * 1024  # 1GB
-    clear_gpu_memory()
-    #wait_until_enough_gpu_memory(min_memory_available)
-
-    st.sidebar.title("Select From The List Below: ")
-    selection = st.sidebar.radio("GO TO: ", ["Document Embedding","RAG Chatbot", ])
+    st.set_page_config(page_title="RAG Chatbot", page_icon="🤖", layout="wide")
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Select a page:", ["Document Embedding", "RAG Chatbot"])
 
     if selection == "Document Embedding":
         display_document_embedding_page()
-
     elif selection == "RAG Chatbot":
         display_chatbot_page()
-   
-
-def display_chatbot_page():
-
-    st.title("Multi Source Chatbot")
-
-    # Setting the LLM
-    with st.expander("Initialize the LLM Model"):
-        st.markdown("""
-            Please Insert the Token and Select Vector Store, Temperature, and Maximum Character Length to create the chatbot.
-
-            **NOTE:**
-            - **Token:** API Key From Hugging Face.
-            - **Temperature:** How much creative the chatbot will be? Don't Insert 0 or More Than 1.""")
-        with st.form("setting"):
-            row_1 = st.columns(3)
-            with row_1[0]:
-                token = st.text_input("Hugging Face Token", type="password")
-
-            with row_1[1]:
-                llm_model = st.text_input("LLM model", value="tiiuae/falcon-7b-instruct")
-
-            with row_1[2]:
-                instruct_embeddings = st.text_input("Instruct Embeddings", value="sentence-transformers/all-MiniLM-L6-v2")
-
-            row_2 = st.columns(3)
-            with row_2[0]:
-                vector_store_list = os.listdir("vector store/")
-                default_choice = (
-                    vector_store_list.index('naruto_snake')
-                    if 'naruto_snake' in vector_store_list
-                    else 0
-                )
-                existing_vector_store = st.selectbox("Vector Store", vector_store_list, default_choice)
-            
-            with row_2[1]:
-                temperature = st.number_input("Temperature", value=1.0, step=0.1)
-
-            with row_2[2]:
-                max_length = st.number_input("Maximum character length", value=300, step=1)
-
-            create_chatbot = st.form_submit_button("Launch chatbot")
-
-
-    # Prepare the LLM model
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-
-    if token:
-        st.session_state.conversation = falcon.prepare_rag_llm(
-            token, existing_vector_store, temperature, max_length
-        )
-
-    # Chat history
-    if "history" not in st.session_state:
-        st.session_state.history = []
-
-    # Source documents
-    if "source" not in st.session_state:
-        st.session_state.source = []
-
-    # Display chats
-    for message in st.session_state.history:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Ask a question
-    if question := st.chat_input("Ask a question"):
-        # Append user question to history
-        st.session_state.history.append({"role": "user", "content": question})
-        # Add user question
-        with st.chat_message("user"):
-            st.markdown(question)
-
-        # Answer the question
-        answer, doc_source = falcon.generate_answer(question, token)
-        with st.chat_message("assistant"):
-            st.write(answer)
-        # Append assistant answer to history
-        st.session_state.history.append({"role": "assistant", "content": answer})
-
-        # Append the document sources
-        st.session_state.source.append({"question": question, "answer": answer, "document": doc_source})
-
-
-    # Source documents
-    with st.expander("Chat History and Source Information"):
-        st.write(st.session_state.source)
 
 def display_document_embedding_page():
-    st.title("Document Embedding Page")
-    st.markdown("""This page is used to upload the documents as the custom knowledge base for the chatbot.
-                  **NOTE:** If you are uploading a new file (for the first time) please insert a new vector store name to store it in vector database
-                """)
+    vector_store_list = [item for item in os.listdir("vector store") if item != ".DS_Store"]
+
+    st.title("Document Embedding")
+    st.markdown("Upload documents to create a custom knowledge base for the chatbot.")
 
     with st.form("document_input"):
+        document = st.file_uploader("Upload Documents (PDF or TXT)", type=['pdf', 'txt'], accept_multiple_files=True)
+        instruct_embeddings = st.text_input("Instruct Embeddings Model", value="sentence-transformers/all-MiniLM-L6-v2")
+        chunk_size = st.number_input("Chunk Size", value=200, min_value=0, step=1)
+        chunk_overlap = st.number_input("Chunk Overlap", value=10, min_value=0, step=1)
         
-        document = st.file_uploader(
-            "Knowledge Documents", type=['pdf', 'txt'], help=".pdf or .txt file", accept_multiple_files= True
-        )
-
-        row_1 = st.columns([2, 1, 1])
-        with row_1[0]:
-            instruct_embeddings = st.text_input(
-                "Model Name of the Instruct Embeddings", value="sentence-transformers/all-MiniLM-L6-v2"
-            )
-        
-        with row_1[1]:
-            chunk_size = st.number_input(
-                "Chunk Size", value=200, min_value=0, step=1,
-            )
-        
-        with row_1[2]:
-            chunk_overlap = st.number_input(
-                "Chunk Overlap", value=10, min_value=0, step=1,
-                help="higher that chunk size"
-            )
-        
-        row_2 = st.columns(2)
-        with row_2[0]:
-            # List the existing vector stores
-            vector_store_list = os.listdir("vector store/")
-            vector_store_list = ["<New>"] + vector_store_list
-            
-            existing_vector_store = st.selectbox(
-                "Vector Store to Merge the Knowledge", vector_store_list,
-                help="""
-                Which vector store to add the new documents.
-                Choose <New> to create a new vector store.
-                    """
-            )
-
-        with row_2[1]:
-            # List the existing vector stores     
-            new_vs_name = st.text_input(
-                "New Vector Store Name", value="new_vector_store_name",
-                help="""
-                If choose <New> in the dropdown / multiselect box,
-                name the new vector store. Otherwise, fill in the existing vector
-                store to merge.
-                """
-            )
-
-        save_button = st.form_submit_button("Save vector store")
+        if not os.path.exists("vector store"):
+            os.makedirs("vector store")
+        vector_store_list = ["<New>"] + vector_store_list
+        existing_vector_store = st.selectbox("Select or Create Vector Store", vector_store_list)
+        new_vs_name = st.text_input("New Vector Store Name", value="new_vector_store")
+        save_button = st.form_submit_button("Save Vector Store")
 
     if save_button:
-        if document is not None:
-            # Aggregate content of all uploaded files
+        if document:
             combined_content = ""
             for file in document:
                 if file.name.endswith(".pdf"):
-                    combined_content += falcon.read_pdf(file)
+                    combined_content += read_pdf(file)
                 elif file.name.endswith(".txt"):
-                    combined_content += falcon.read_txt(file)
-                else:
-                    st.error("Check if the uploaded file is .pdf or .txt")
+                    combined_content += read_txt(file)
 
-            # Split combined content into chunks
-            split = falcon.split_doc(combined_content, chunk_size, chunk_overlap)
+            split = split_doc(combined_content, chunk_size, chunk_overlap)
+            create_new_vs = existing_vector_store == "<New>"
 
-            # Check whether to create new vector store
-            create_new_vs = None
-            if existing_vector_store == "<New>" and new_vs_name != "":
-                create_new_vs = True
-            elif existing_vector_store != "<New>" and new_vs_name != "":
-                create_new_vs = False
-            else:
-                st.error("Check the 'Vector Store to Merge the Knowledge' and 'New Vector Store Name'")
-
-            # Embeddings and storing
-            falcon.embedding_storing(split, create_new_vs, existing_vector_store, new_vs_name)
-            print(f'"Document info":{combined_content}')    
-            print(f'"Splitted info":{split}')   
-
+            try:
+                embedding_storing(split, create_new_vs, existing_vector_store, new_vs_name)
+                st.success("Vector store saved successfully!")
+            except ValueError as e:
+                st.error(f"Error: {e}")
         else:
             st.warning("Please upload at least one file.")
 
+def display_chatbot_page():
+    st.title("Multi Source Chatbot")
 
+    with st.expander("Initialize the LLM Model"):
+        st.markdown("""
+            Insert the token and select the vector store, temperature, and maximum character length to create the chatbot.
+            **NOTE:** Token: API Key from Hugging Face. Temperature: How creative should the chatbot be? (Value between 0 and 1)
+        """)
+
+        with st.form("settings"):
+            api_key = load_secrets()
+            if api_key:
+                token = st.text_input("Hugging Face Token (No need to insert)", type='password', value="******")
+            else:
+                token = st.text_input("Hugging Face Token (No need to insert)", type='password')
+            llm_model = st.text_input("LLM Model", value="tiiuae/falcon-7b-instruct")
+            instruct_embeddings = st.text_input("Instruct Embeddings", value="sentence-transformers/all-MiniLM-L6-v2")
+            vector_store_list = os.listdir("vector store")
+            default_choice = vector_store_list.index('naruto_snake') if 'naruto_snake' in vector_store_list else 0
+            existing_vector_store = st.selectbox("Vector Store", vector_store_list, index=default_choice)
+            temperature = st.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7, step=0.1)
+            max_length = st.number_input("Maximum Character Length", value=200, min_value=1, step=1)
+            create_chatbot = st.form_submit_button("Initialize Chatbot")
+
+    if create_chatbot:
+        if api_key:
+            st.session_state.conversation = prepare_rag_llm(api_key, existing_vector_store, temperature, max_length)
+            st.success("Chatbot initialized successfully!")
+        else:
+            st.error("Please enter a valid API key.")
+
+    st.markdown("### Chat with the Bot")
+    st.markdown("Enter your text below to get a response from the chatbot. **NOTE:** Initialize the LLM Model above before using the chatbot.")
+
+    if 'conversation' in st.session_state:
+        with st.form("chat", clear_on_submit=True):
+            user_input = st.text_area("Your Question:", value="", height=150)
+            submit_button = st.form_submit_button("Send")
+
+        if submit_button and user_input:
+            with st.spinner("Generating response..."):
+                answer, doc_source = generate_answer(user_input, api_key)
+
+            st.markdown(f"**You:** {user_input}")
+            st.markdown(f"**Chatbot:** {answer}")
+
+            st.markdown("### Source Documents")
+            for idx, source in enumerate(doc_source, 1):
+                st.markdown(f"**{idx}.** {source}")
+    else:
+        st.warning("Please initialize the chatbot first.")
 
 if __name__ == "__main__":
     main()
