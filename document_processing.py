ab46200d85724739e33352df9a3be60fcc2e825e
Ehsan Taheri <ehsan.taheri1400@gmail.com>
2024-06-19 23:44:59 +0200

    Update document_processing.py

--- document_processing.py
+++ document_processing.py
@@ -1,4 +1,6 @@
 import os
+import requests
+from bs4 import BeautifulSoup
 from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -17,12 +19,23 @@ def read_txt(file):
     document = str(file.read(), 'utf-8')
     return document
 
+def fetch_url_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check for request errors
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Extract text from the page
+        text = ' '.join(p.get_text() for p in soup.find_all('p'))
+        return text
+    except requests.RequestException as e:
+        st.error(f"Failed to fetch the content from URL: {e}")
+        return ""
+
 def split_doc(document, chunk_size, chunk_overlap):
     splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     split = splitter.split_text(document)
     split = splitter.create_documents(split)
     return split
-
 
 def embedding_storing(split, create_new_vs, existing_vector_store, new_vs_name):
     instructor_embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
@@ -38,4 +51,3 @@ def embedding_storing(split, create_new_vs, existing_vector_store, new_vs_name):
         load_db.save_local(os.path.join("vector store", existing_vector_store))
 
     st.success("The document has been saved.")
-
